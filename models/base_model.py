edfe8d60dd32a3ef4a02bdd830f15aca8f75c473
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-03-17 16:54:30 +0200

    feat: Created Base And User Models.

---
+++ models/base_model.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+"""Base model for all models in the application"""
+
+import models
+from uuid import uuid4
+from os import getenv
+from datetime import datetime
+import sqlalchemy
+from sqlalchemy import Column, Integer, DateTime, String
+from sqlalchemy.ext.declarative import declarative_base
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+Base = declarative_base()
+
+
+class BaseModel(Base):
+    """Base model for all models in the application"""
+
+    __abstract__ = True
+
+    id = Column(String(60), primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the base model"""
+        if kwargs:
+            for k, v in kwargs.items():
+                if k != "__class__":
+                    setattr(self, k, v)
+
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid4())
+
+            self.created_at = datetime.strptime(
+                self.created_at, time
+            ) if "created_at" in kwargs else datetime.now()
+
+            self.updated_at = datetime.strptime(
+                self.updated_at, time
+            ) if "updated_at" in kwargs else datetime.now()
+
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+
+    def save(self):
+        """Save the model to the database"""
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
+
+    def delete(self):
+        """Delete the model from the database"""
+        models.storage.delete(self)
+
+    def to_dict(self):
+        """Return a dictionary representation of the model"""
+        d = dict(self.__dict__).copy()
+        if "_sa_instance_state" in d:
+            del d["_sa_instance_state"]
+        if "password" in d:
+            del d["password"]
+        if "created_at" in d:
+            d["created_at"] = d["created_at"].strftime(time)
+        if "updated_at" in d:
+            d["updated_at"] = d["updated_at"].strftime(time)
+        return d
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return "[{:s}] ({}) {}".format(self.__class__.__name__, self.id,
+                                       self.to_dict())
---
+++ models/user.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""This is the user module."""
+
+import models
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+import bcrypt
+
+
+class User(BaseModel, Base):
+    """User class to interact with the API."""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    review = relationship("Review", backref="user")
+    
+    def __init__(self, *args, **kwargs):
+        """User class constructor"""
+        super().__init__(*args, **kwargs)
+
+    def __setattr__(self, k, v):
+        """Hash the password before setting it."""
+        v = bcrypt.hashpw(
+            v.encode(), bcrypt.gensalt()
+            ) if k == 'password' else super().__setattr__(k, v)
