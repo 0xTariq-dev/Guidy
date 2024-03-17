d4af4487ff5cf03b230dfba355d05631e6c10be0
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-03-17 17:59:17 +0200

    feat: Created Resource And Review Models.

---
+++ models/review.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+"""Review module"""
+
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+
+
+class Review(BaseModel, Base):
+    """Review class to store review information"""
+    __tablename__ = 'reviews'
+    text = Column(String(1024), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    course_id = Column(String(60), ForeignKey('courses.id'), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
