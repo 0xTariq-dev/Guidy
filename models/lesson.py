a533174bcaa9e8807fdcd94a5bf23d74611efe45
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-04-02 15:38:24 +0200

    Enhancement: Expanded lesson description size to accommodate longer descriptions

--- models/lesson.py
+++ models/lesson.py
@@ -11,7 +11,7 @@ class Lesson(BaseModel, Base):
     """Lesson class to store lesson information"""
     __tablename__ = 'lessons'
     title = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(4096), nullable=False)
     course_id = Column(String(60), ForeignKey('courses.id'), nullable=False)
     course = relationship("Course", back_populates="lessons")
     resources = relationship("Resource", back_populates="lesson")
